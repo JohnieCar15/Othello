ec5bc1b6365c21ae012e03a0313d345417fd6814
JohnieCar15 <94120002+JohnieCar15@users.noreply.github.com>
2022-08-21 23:18:22 +1000

    Added winning logic

--- main.py
+++ main.py
@@ -24,7 +24,56 @@ SCREEN.fill(BG_COLOR)
 SCREEN.blit(BOARD, (64, 64))
  
 pygame.display.update()
- 
+
+def check_win(board):
+    winner = None
+    for row in range(0, 3):
+        if((board[row][0] == board[row][1] == board[row][2]) and (board [row][0] is not None)):
+            winner = board[row][0]
+            for i in range(0, 3):
+                graphical_board[row][i][0] = pygame.image.load(f"assets/Winning {winner}.png")
+                SCREEN.blit(graphical_board[row][i][0], graphical_board[row][i][1])
+            pygame.display.update()
+            return winner
+
+    for col in range(0, 3):
+        if((board[0][col] == board[1][col] == board[2][col]) and (board[0][col] is not None)):
+            winner =  board[0][col]
+            for i in range(0, 3):
+                graphical_board[i][col][0] = pygame.image.load(f"assets/Winning {winner}.png")
+                SCREEN.blit(graphical_board[i][col][0], graphical_board[i][col][1])
+            pygame.display.update()
+            return winner
+   
+    if (board[0][0] == board[1][1] == board[2][2]) and (board[0][0] is not None):
+        winner =  board[0][0]
+        graphical_board[0][0][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[0][0][0], graphical_board[0][0][1])
+        graphical_board[1][1][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[1][1][0], graphical_board[1][1][1])
+        graphical_board[2][2][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[2][2][0], graphical_board[2][2][1])
+        pygame.display.update()
+        return winner
+          
+    if (board[0][2] == board[1][1] == board[2][0]) and (board[0][2] is not None):
+        winner =  board[0][2]
+        graphical_board[0][2][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[0][2][0], graphical_board[0][2][1])
+        graphical_board[1][1][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[1][1][0], graphical_board[1][1][1])
+        graphical_board[2][0][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[2][0][0], graphical_board[2][0][1])
+        pygame.display.update()
+        return winner
+    
+    if winner is None:
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] != 'X' and board[i][j] != 'O':
+                    return None
+        return "DRAW"
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
