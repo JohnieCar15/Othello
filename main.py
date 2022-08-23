e0ec59ef1fbafc26aeef3f71c4a4fbeda4048db6
johnie <johniecarino15@gmail.com>
2022-08-23 21:20:42 +1000

    Added rescaling to window

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ pygame.init()
  
 WIDTH, HEIGHT = 900, 900
  
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), flags=pygame.SCALED)
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), flags=pygame.RESIZABLE|pygame.DOUBLEBUF|pygame.HWSURFACE)
 pygame.display.set_caption("Tic Tac Toe!")
  
 BOARD = pygame.image.load("assets/Board.png")
@@ -21,61 +21,22 @@ graphical_board = [[[None, None], [None, None], [None, None]],
 to_move = 'X'
  
 SCREEN.fill(BG_COLOR)
-SCREEN.blit(BOARD, (64, 64))
+SCREEN.blit(pygame.transform.scale(BOARD, (WIDTH, HEIGHT)), (0, 0))
  
 pygame.display.update()
 
-def check_win(board):
-    winner = None
-    for row in range(0, 3):
-        if((board[row][0] == board[row][1] == board[row][2]) and (board [row][0] is not None)):
-            winner = board[row][0]
-            for i in range(0, 3):
-                graphical_board[row][i][0] = pygame.image.load(f"assets/Winning {winner}.png")
-                SCREEN.blit(graphical_board[row][i][0], graphical_board[row][i][1])
-            pygame.display.update()
-            return winner
-
-    for col in range(0, 3):
-        if((board[0][col] == board[1][col] == board[2][col]) and (board[0][col] is not None)):
-            winner =  board[0][col]
-            for i in range(0, 3):
-                graphical_board[i][col][0] = pygame.image.load(f"assets/Winning {winner}.png")
-                SCREEN.blit(graphical_board[i][col][0], graphical_board[i][col][1])
-            pygame.display.update()
-            return winner
-   
-    if (board[0][0] == board[1][1] == board[2][2]) and (board[0][0] is not None):
-        winner =  board[0][0]
-        graphical_board[0][0][0] = pygame.image.load(f"assets/Winning {winner}.png")
-        SCREEN.blit(graphical_board[0][0][0], graphical_board[0][0][1])
-        graphical_board[1][1][0] = pygame.image.load(f"assets/Winning {winner}.png")
-        SCREEN.blit(graphical_board[1][1][0], graphical_board[1][1][1])
-        graphical_board[2][2][0] = pygame.image.load(f"assets/Winning {winner}.png")
-        SCREEN.blit(graphical_board[2][2][0], graphical_board[2][2][1])
-        pygame.display.update()
-        return winner
-          
-    if (board[0][2] == board[1][1] == board[2][0]) and (board[0][2] is not None):
-        winner =  board[0][2]
-        graphical_board[0][2][0] = pygame.image.load(f"assets/Winning {winner}.png")
-        SCREEN.blit(graphical_board[0][2][0], graphical_board[0][2][1])
-        graphical_board[1][1][0] = pygame.image.load(f"assets/Winning {winner}.png")
-        SCREEN.blit(graphical_board[1][1][0], graphical_board[1][1][1])
-        graphical_board[2][0][0] = pygame.image.load(f"assets/Winning {winner}.png")
-        SCREEN.blit(graphical_board[2][0][0], graphical_board[2][0][1])
-        pygame.display.update()
-        return winner
-    
-    if winner is None:
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if board[i][j] != 'X' and board[i][j] != 'O':
-                    return None
-        return "DRAW"
+def adjust_mouse_pos():
+    print(pygame.mouse.get_pos())
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.VIDEORESIZE:
+            SCREEN = pygame.display.set_mode(event.size, flags=pygame.RESIZABLE|pygame.DOUBLEBUF|pygame.HWSURFACE)
+            SCREEN.fill(BG_COLOR)
+            SCREEN.blit(pygame.transform.scale(BOARD, event.size), (0, 0))
+            pygame.display.update()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())
