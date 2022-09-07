ea96adfb6a0202a53350d2cd9f94042ce17338c1
johnie <johniecarino15@gmail.com>
2022-09-07 21:40:21 +1000

    Continued making changes to monte carlo search algorithm

--- othello.py
+++ othello.py
@@ -3,7 +3,7 @@
 import pygame, sys
 import math
 
-from othelloai import get_score, get_possible_moves, alphabeta, is_finished
+from othelloai import get_score, get_possible_moves, alphabeta, is_finished, flip_pieces, MonteCarloTreeSearchNode
  
 pygame.init()
 
@@ -35,190 +35,6 @@ def render_board():
                 color = (200, 200, 200) if board[x][y] == "W" else (0, 0, 0)
                 pygame.draw.circle(SCREEN, color, (y * (HEIGHT / 8) + HEIGHT / 16, x * (WIDTH / 8) + WIDTH / 16), 30)  
 
-def flip_pieces(y_pos, x_pos, to_move):
-    global board
-
-    up = y_pos - 1
-    down = y_pos + 1
-    left = x_pos - 1
-    right = x_pos + 1
-
-    is_legal = False
-
-    can_flip = False
-
-    # Vertically upwards direction
-    while up != -1:
-        if board[up][x_pos] == None:
-            break
-        elif board[up][x_pos] != to_move:
-            can_flip = True
-
-        if board[up][x_pos] == to_move and can_flip == False:
-            break
-
-        if board[up][x_pos] == to_move and can_flip == True:
-            is_legal = True
-            for x in range(up, y_pos):
-                board[x][x_pos] = to_move
-            break
-        up -= 1
-
-    up = y_pos - 1
-    can_flip = False
-
-    # Vertically downwards direction
-    while down != 8:
-        if board[down][x_pos] == None:
-            break
-        elif board[down][x_pos] != to_move:
-            can_flip = True
-
-
-        if board[down][x_pos] == to_move and can_flip == False:
-            break
-
-        if board[down][x_pos] == to_move and can_flip == True:
-            is_legal = True
-            for x in range(y_pos, down):
-                board[x][x_pos] = to_move
-            break
-        down += 1
-
-    down = y_pos + 1
-    can_flip = False
-
-    # Horizontally to the left direction
-    while left != -1:
-        if board[y_pos][left] == None:
-            break
-        elif board[y_pos][left] != to_move:
-            can_flip = True
-
-        if board[y_pos][left] == to_move and can_flip == False:
-            break
-
-        if board[y_pos][left] == to_move and can_flip == True:
-            is_legal = True
-            for x in range(left, x_pos):
-                board[y_pos][x] = to_move
-            break
-        left -= 1
-
-    left = x_pos - 1
-    can_flip = False
-
-    # Horizontally to the right direction
-    while right != 8:
-        if board[y_pos][right] == None:
-            break
-        elif board[y_pos][right] != to_move:
-            can_flip = True
-
-        if board[y_pos][right] == to_move and can_flip == False:
-            break
-
-        if board[y_pos][right] == to_move and can_flip == True:
-            is_legal = True
-            for x in range(x_pos, right):
-                board[y_pos][x] = to_move
-            break
-        right += 1
-    
-    right = x_pos + 1
-    can_flip = False
-
-    # Diagonally to the top left direction
-    while up != -1 and left != -1:
-        if board[up][left] == None:
-            break
-        elif board[up][left] != to_move:
-            can_flip = True
-
-        if board[up][left] == to_move and can_flip == False:
-            break
-
-        if board[up][left] == to_move and can_flip == True:
-            is_legal = True
-            for x in range(up, y_pos):
-                board[x][left] = to_move
-                left += 1
-            break
-        up -= 1
-        left -= 1
-    
-    up = y_pos - 1
-    left = x_pos - 1
-    can_flip = False
-
-    # Diagonally to the top right direction
-    while up != -1 and right != 8:
-        if board[up][right] == None:
-            break
-        elif board[up][right] != to_move:
-            can_flip = True
-
-        if board[up][right] == to_move and can_flip == False:
-            break
-
-        if board[up][right] == to_move and can_flip == True:
-            is_legal = True
-            for x in range(up, y_pos):
-                board[x][right] = to_move
-                right -= 1
-            break
-        up -= 1
-        right += 1
-    
-    right = x_pos + 1
-
-    can_flip = False
-
-    # Diagonally to the bottom right direction
-    while down != 8 and right != 8:
-        if board[down][right] == None:
-            break
-        elif board[down][right] != to_move:
-            can_flip = True
-
-
-        if board[down][right] == to_move and can_flip == False:
-            break
-
-        if board[down][right] == to_move and can_flip == True:
-            is_legal = True
-            for x in range(down, y_pos, -1):
-                board[x][right] = to_move
-                right -= 1
-            break
-        down += 1
-        right += 1
-    
-    down = y_pos + 1
-
-    can_flip = False
-
-    # Diagonally to the bottom left direction
-    while down != 8 and left != -1:
-        if board[down][left] == None:
-            break
-        elif board[down][left] != to_move:
-            can_flip = True
-
-        if board[down][left] == to_move and can_flip == False:
-            break
-
-        if board[down][left] == to_move and can_flip == True:
-            is_legal = True
-            for x in range(down, y_pos, -1):
-                board[x][left] = to_move
-                left += 1
-            break
-        down += 1
-        left -= 1
-    
-    return is_legal
-    
 
 def adjust_board(to_move):
     global board
@@ -234,7 +50,7 @@ def adjust_board(to_move):
             return
 
     if board[y_pos][x_pos] == None:
-        if not flip_pieces(y_pos, x_pos, to_move):
+        if not flip_pieces(y_pos, x_pos, to_move, board):
             return to_move
         board[y_pos][x_pos] = to_move
         
@@ -260,9 +76,11 @@ while True:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if to_move == "B":
-                coordinates = alphabeta(board, 5, -math.inf, math.inf, True)[0]
-                flip_pieces(coordinates[0], coordinates[1], to_move)
-                board[coordinates[0]][coordinates[1]] = "B"
+                # coordinates = alphabeta(board, 5, -math.inf, math.inf, True)[0]
+                # flip_pieces(coordinates[0], coordinates[1], to_move, board)
+                # board[coordinates[0]][coordinates[1]] = "B"
+                root = MonteCarloTreeSearchNode(state = board)
+                selected_node = root.best_action()
                 render_board()
                 to_move = "W"
             else:
--- othelloai.py
+++ othelloai.py
@@ -152,6 +152,51 @@ class MonteCarloTreeSearchNode():
         '''
         return get_possible_moves(self.state)
 
+    def is_game_over(self):
+        '''
+        Modify according to your game or 
+        needs. It is the game over condition
+        and depends on your game. Returns
+        true or false
+        '''
+        return is_finished(self.state)
+
+    def game_result(self):
+        '''
+        Modify according to your game or 
+        needs. Returns 1 or 0 or -1 depending
+        on your state corresponding to win,
+        tie or a loss.
+        '''
+
+        total_scores = get_score(self.state)
+        if total_scores["B"] > total_scores["W"]:
+            return 1
+        elif total_scores["W"] > total_scores["B"]:
+            return -1
+        else:
+            return 0
+
+    def move(self,action):
+        '''
+        Modify according to your game or 
+        needs. Changes the state of your 
+        board with a new value. For a normal
+        Tic Tac Toe game, it can be a 3 by 3
+        array with all the elements of array
+        being 0 initially. 0 means the board 
+        position is empty. If you place x in
+        row 2 column 3, then it would be some 
+        thing like board[2][3] = 1, where 1
+        represents that x is placed. Returns 
+        the new state after making a move.
+        '''
+
+        flip_pieces(action[1], action[0], "B", self.state)
+        self.state[action[1]][action[0]] = "B"
+        
+        return self.state
+
 def is_finished(board):
     if len(get_possible_moves(board, "B")) == 0 and len(get_possible_moves(board, "W")) == 0:
         return True
@@ -351,3 +396,185 @@ def get_possible_moves(board, to_move):
     
     return moves
 
+def flip_pieces(y_pos, x_pos, to_move, board):
+
+    up = y_pos - 1
+    down = y_pos + 1
+    left = x_pos - 1
+    right = x_pos + 1
+
+    is_legal = False
+
+    can_flip = False
+
+    # Vertically upwards direction
+    while up != -1:
+        if board[up][x_pos] == None:
+            break
+        elif board[up][x_pos] != to_move:
+            can_flip = True
+
+        if board[up][x_pos] == to_move and can_flip == False:
+            break
+
+        if board[up][x_pos] == to_move and can_flip == True:
+            is_legal = True
+            for x in range(up, y_pos):
+                board[x][x_pos] = to_move
+            break
+        up -= 1
+
+    up = y_pos - 1
+    can_flip = False
+
+    # Vertically downwards direction
+    while down != 8:
+        if board[down][x_pos] == None:
+            break
+        elif board[down][x_pos] != to_move:
+            can_flip = True
+
+
+        if board[down][x_pos] == to_move and can_flip == False:
+            break
+
+        if board[down][x_pos] == to_move and can_flip == True:
+            is_legal = True
+            for x in range(y_pos, down):
+                board[x][x_pos] = to_move
+            break
+        down += 1
+
+    down = y_pos + 1
+    can_flip = False
+
+    # Horizontally to the left direction
+    while left != -1:
+        if board[y_pos][left] == None:
+            break
+        elif board[y_pos][left] != to_move:
+            can_flip = True
+
+        if board[y_pos][left] == to_move and can_flip == False:
+            break
+
+        if board[y_pos][left] == to_move and can_flip == True:
+            is_legal = True
+            for x in range(left, x_pos):
+                board[y_pos][x] = to_move
+            break
+        left -= 1
+
+    left = x_pos - 1
+    can_flip = False
+
+    # Horizontally to the right direction
+    while right != 8:
+        if board[y_pos][right] == None:
+            break
+        elif board[y_pos][right] != to_move:
+            can_flip = True
+
+        if board[y_pos][right] == to_move and can_flip == False:
+            break
+
+        if board[y_pos][right] == to_move and can_flip == True:
+            is_legal = True
+            for x in range(x_pos, right):
+                board[y_pos][x] = to_move
+            break
+        right += 1
+    
+    right = x_pos + 1
+    can_flip = False
+
+    # Diagonally to the top left direction
+    while up != -1 and left != -1:
+        if board[up][left] == None:
+            break
+        elif board[up][left] != to_move:
+            can_flip = True
+
+        if board[up][left] == to_move and can_flip == False:
+            break
+
+        if board[up][left] == to_move and can_flip == True:
+            is_legal = True
+            for x in range(up, y_pos):
+                board[x][left] = to_move
+                left += 1
+            break
+        up -= 1
+        left -= 1
+    
+    up = y_pos - 1
+    left = x_pos - 1
+    can_flip = False
+
+    # Diagonally to the top right direction
+    while up != -1 and right != 8:
+        if board[up][right] == None:
+            break
+        elif board[up][right] != to_move:
+            can_flip = True
+
+        if board[up][right] == to_move and can_flip == False:
+            break
+
+        if board[up][right] == to_move and can_flip == True:
+            is_legal = True
+            for x in range(up, y_pos):
+                board[x][right] = to_move
+                right -= 1
+            break
+        up -= 1
+        right += 1
+    
+    right = x_pos + 1
+
+    can_flip = False
+
+    # Diagonally to the bottom right direction
+    while down != 8 and right != 8:
+        if board[down][right] == None:
+            break
+        elif board[down][right] != to_move:
+            can_flip = True
+
+
+        if board[down][right] == to_move and can_flip == False:
+            break
+
+        if board[down][right] == to_move and can_flip == True:
+            is_legal = True
+            for x in range(down, y_pos, -1):
+                board[x][right] = to_move
+                right -= 1
+            break
+        down += 1
+        right += 1
+    
+    down = y_pos + 1
+
+    can_flip = False
+
+    # Diagonally to the bottom left direction
+    while down != 8 and left != -1:
+        if board[down][left] == None:
+            break
+        elif board[down][left] != to_move:
+            can_flip = True
+
+        if board[down][left] == to_move and can_flip == False:
+            break
+
+        if board[down][left] == to_move and can_flip == True:
+            is_legal = True
+            for x in range(down, y_pos, -1):
+                board[x][left] = to_move
+                left += 1
+            break
+        down += 1
+        left -= 1
+    
+    return is_legal
